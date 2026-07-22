cf428123d9a76e34af44cbd3763cbd7a2df8f679
Bobby Pamarthi <bhavanipmarthi@gmail.com>
2026-07-22 14:31:50 +0530

    Import cloudguardian files

---
+++ infra/azure-3tier/database.tf
@@ -0,0 +1,53 @@
+resource "azurerm_mssql_server" "main" {
+  name                          = "${local.name}-sql-${random_string.suffix.result}"
+  resource_group_name           = azurerm_resource_group.main.name
+  location                      = azurerm_resource_group.main.location
+  version                       = "12.0"
+  administrator_login           = var.sql_admin_login
+  administrator_login_password  = var.sql_admin_password
+  minimum_tls_version           = "1.2"
+  public_network_access_enabled = false
+  tags                          = local.tags
+}
+
+resource "azurerm_mssql_database" "main" {
+  name        = "${local.name}-db"
+  server_id   = azurerm_mssql_server.main.id
+  sku_name    = "Basic"
+  max_size_gb = 2
+  tags        = local.tags
+}
+
+resource "azurerm_private_dns_zone" "sql" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.main.name
+  tags                = local.tags
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sql" {
+  name                  = "sql-dns-link"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.sql.name
+  virtual_network_id    = azurerm_virtual_network.main.id
+  tags                  = local.tags
+}
+
+resource "azurerm_private_endpoint" "sql" {
+  name                = "${local.name}-sql-pe"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.data.id
+  tags                = local.tags
+
+  private_service_connection {
+    name                           = "sql-psc"
+    private_connection_resource_id = azurerm_mssql_server.main.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
+
+  private_dns_zone_group {
+    name                 = "sql-dns-zone-group"
+    private_dns_zone_ids = [azurerm_private_dns_zone.sql.id]
+  }
+}
